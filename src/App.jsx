46943d24ec6a7c02a20483c80c13f02622167df1
brendonrodri <rodriguesbrendon861@gmail.com>
2023-03-04 14:50:25 -0300

    contexto criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import ContextProvider from "./Services/Context/context"
 export default function App (){
     return(
-        <>
+        <ContextProvider>
             
-        </>
+        </ContextProvider>
     )
 }
