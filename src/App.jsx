b7d3eaf012f8d4fc2559f5ee6b21d8857ed8b4f9
brendonrodri <rodriguesbrendon861@gmail.com>
2023-03-04 23:57:19 -0300

    feat: adicionada requisição da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import GetApi from "./Services/API/MovieAPI"
 import ContextProvider from "./Services/Context/context"
+import GlobalStyle from "./Services/GlobalStyle/style"
 export default function App (){
     return(
         <ContextProvider>
-            
+            <GlobalStyle />
+            <GetApi />
         </ContextProvider>
     )
 }
--- src/Services/API/MovieAPI/index.js
+++ src/Services/API/MovieAPI/index.js
@@ -1,5 +1,7 @@
-import React from "react";
 import axios from "axios";
 const GetApi = ()=>{
-    axios.get({})   
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=2785fd17cabc7d3339d5a4671f60e34f&language=pt-BR&page=2`).then((response)=>{
+        console.log(response.data)
+    })  
 }
+export default GetApi;
