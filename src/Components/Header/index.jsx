3e9fc0a45043230e0c35d630946078c84f9aeb54
brendonrodri <rodriguesbrendon861@gmail.com>
2023-03-07 19:51:12 -0300

    debug

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { LogoHeaderComponent } from "../LogoHeader"
 import NavBarComponent from "../NavBarComponent"
 import SearchBarInput from "../SearchBarComponent"
-import { SearchBarContainer } from "../SearchBarComponent/style"
 import * as S from "./style"
 export default function HeaderComponent(){
     return(
--- src/Services/API/MovieAPI/index.js
+++ src/Services/API/MovieAPI/index.js
@@ -7,7 +7,7 @@ const GetApi = ()=>{
             axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=2785fd17cabc7d3339d5a4671f60e34f&language=pt-BR&page=2`).then((response)=>{
                 setApiData(response.data.results)
         })
-        },[])
+        },[setApiData])
         return
 }
 export default GetApi;
