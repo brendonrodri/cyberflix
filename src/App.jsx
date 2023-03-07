4b7818bfff39160a7e97ba1c50118c7298c63d01
brendonrodri <rodriguesbrendon861@gmail.com>
2023-03-07 19:53:48 -0300

    fix: removido importações não ultilizadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import ComponentData from "./Components/SlideComponent/componentData"
 import GetApi from "./Services/API/MovieAPI"
 import GlobalStyle from "./Services/GlobalStyle/style"
 import HelloPage from "./Services/Pages/Index"
