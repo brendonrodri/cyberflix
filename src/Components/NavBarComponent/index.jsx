e4e595803f87c62d627ff1ed42ef3d68c5dffd97
brendonrodri <rodriguesbrendon861@gmail.com>
2023-05-05 14:19:37 -0300

    limpando input ao clicar em uma rota

--- src/Components/NavBarComponent/index.jsx
+++ src/Components/NavBarComponent/index.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { Context } from "../../Services/Context/context"
 import * as S from "./style"
 export default function NavBarComponent() {
+    const {setInput} = useContext(Context)
+    const clearInput = () => setInput('')
     return (
         <>
             <S.NavBar>
                 <S.NavList>
-                    <S.NavListItem><Link to="/">Início</Link></S.NavListItem>
-                    <S.NavListItem><Link to="/Populares"> Populares </Link> </S.NavListItem>
-                    <S.NavListItem><Link to="/Series">Séries</Link></S.NavListItem>
-                    <S.NavListItem>Gêneros </S.NavListItem>
+                    <S.NavListItem onClick={()=> clearInput()}><Link to="/">Início</Link></S.NavListItem>
+                    <S.NavListItem onClick={()=> clearInput()}><Link to="/Populares"> Populares </Link> </S.NavListItem>
+                    <S.NavListItem onClick={()=> clearInput()}><Link to="/Series">Séries</Link></S.NavListItem>
+                    <S.NavListItem onClick={()=> clearInput()}>Gêneros </S.NavListItem>
                 </S.NavList>
             </S.NavBar>
         </>
